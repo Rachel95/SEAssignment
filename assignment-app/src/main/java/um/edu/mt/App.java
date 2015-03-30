cb7a311d6208f299a1e08587748c5ef18445923c
aray894 <aray894@gmail.com>
2015-03-30 11:55:46 +0200

    Adding Assignment-app
    Adding the Maven project to repo

---
+++ assignment-app/src/main/java/um/edu/mt/App.java
@@ -0,0 +1,13 @@
+package um.edu.mt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
